5d392dcfa6f3c60591936035135c99b331be9a90
Gonzalo Pauchard <Gonzalo Pauchard@10.8.72.100>
2018-04-02 11:55:51 -0300

    Comienzo de interfaz

--- ProyectoProgra/src/Ventanas/MainWindow.java
+++ ProyectoProgra/src/Ventanas/MainWindow.java
@@ -41,7 +41,7 @@ public class MainWindow extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setBackground(Color.DARK_GRAY);
+		contentPane.setBackground(Color.PINK);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
