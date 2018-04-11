08858c65a1f98898924e539a5934f3985b653187
Marhabaaa <37920925+Marhabaaa@users.noreply.github.com>
2018-04-11 15:03:58 -0300

    Clase List agregada

--- ProyectoProgra/src/clases/List.java
+++ ProyectoProgra/src/clases/List.java
@@ -1,5 +1,24 @@
 package clases;
 
-public class List {
+import java.util.ArrayList;
 
+public class List {
+	private ArrayList<Object> list;
+	
+	public List() {
+		list = new ArrayList<>();
+	}
+	
+	public boolean add(Object o) {
+		list.add(o);
+		return true;
+	}
+	
+	public Object get(int index) {
+		return list.get(index);
+	}
+	
+	public int size() {
+		return list.size();
+	}
 }
