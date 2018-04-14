9ef34995423e95b5a3b7b5fe1696f8b3ac1d49d3
Cote <Cote@DESKTOP-2NCMIO5>
2018-04-14 17:17:59 -0300

    CAmbio en la forma de abrir el archivo

--- ProyectoProgra/src/clases/Report.java
+++ ProyectoProgra/src/clases/Report.java
@@ -9,27 +9,26 @@ public class Report {
 	
 
 
-public void ganaciasTotales(SList ordenes){
-	Orden o;
-	int suma;
-	File f= new File ("Reporte.txt");
-	if(f.exists()) {
-		BufferedWriter bw = new BufferedWriter(new FileWriter("Reporte.txt"));
-		bw.write("    Numero de orden 	    	Tecnico			Ganacia por orden \n");
-	
-		for (int i=0;i<ordenes.size();i++) {
-			o = (Orden)ordenes.get(i);
-			if(o.isDone()) {
-			   bw.write(""+i+" " + o.getOrderNumber()+"		" + o.getTecNumber()+ "		" + o.getPrice()+"\n");
+	public void ganaciasTotales(SList ordenes){
+		Orden o;
+		int suma;
+		File f= new File ("Reporte.txt");
+		if(f.exists()) {
+			BufferedWriter bw = new BufferedWriter(new FileWriter("Reporte.txt"));
+			bw.write("    Numero de orden 	    	Tecnico			Ganacia por orden \n");
+		
+			for (int i=0;i<ordenes.size();i++) {
+				o = (Orden)ordenes.get(i);
+				if(o.isDone()) {
+				   bw.write(""+i+" " + o.getOrderNumber()+"		" + o.getTecNumber()+ "		" + o.getPrice()+"\n");
+				}
 			}
 		}
-
+		bw.close();	
+	}
+	
+	public void ganaciasTotalesPantalla(Slist ordenes) {
 		
 	}
-	bw.close();
-	
-	
-}
-
 
 }
